3f60bbbc8304705360676f61409db61125a01068
kamsur <kamsur.rahaman@gmail.com>
2025-04-24 15:53:20 +0200

    updated tests for components
    Signed-off-by: Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>

--- BitAndBeam/frontend/src/app/pages/home/home.component.spec.ts
+++ BitAndBeam/frontend/src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,9 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HomeComponent } from './home.component';
+import { UploadFileComponent } from '../../components/upload-file/upload-file.component';
+import { CommonModule } from '@angular/common'; // Import CommonModule for ngFor, etc.
+import { RouterModule } from '@angular/router'; // Import RouterModule for routing
+import { By } from '@angular/platform-browser'; // Import By for query selector
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,9 +11,8 @@ describe('HomeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HomeComponent]
-    })
-    .compileComponents();
+      imports: [CommonModule, RouterModule, HomeComponent, UploadFileComponent], // Import both HomeComponent and UploadFileComponent as standalone components
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
@@ -19,5 +21,13 @@ describe('HomeComponent', () => {
 
   it('should create HomeComponent', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain the upload-file component', () => {
+    // Find the upload-file component in the template
+    const uploadFileComponent = fixture.debugElement.query(By.css('app-upload-file'));
+
+    // Assert that the upload-file component is found
+    expect(uploadFileComponent).toBeTruthy();
   });
 });
