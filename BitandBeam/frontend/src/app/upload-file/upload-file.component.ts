36e38366c5d93a0dfcbc48f68d73ec9c08ded004
eugenie Bastiane Djamen <bastiane@MacBook-Pro-von-eugenie.local>
2025-04-26 18:55:07 +0200

    Setup minimal Angular app with Home and UploadFile components

---
+++ BitandBeam/frontend/src/app/upload-file/upload-file.component.ts
@@ -0,0 +1,42 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-upload-file',
+  standalone: true,
+  imports: [],
+  templateUrl: './upload-file.component.html',
+  styleUrl: './upload-file.component.css'
+})
+export class UploadFileComponent {
+  files: File[] = [];
+  isDragOver: boolean = false;
+
+  onFileSelected(event: any) {
+    const selectedFiles = event.target.files;
+    if (selectedFiles.length > 0) {
+      for (let i = 0; i < selectedFiles.length; i++) {
+        this.files.push(selectedFiles[i]);
+      }
+    }
+  }
+
+  onFileDropped(event: DragEvent) {
+    event.preventDefault();
+    this.isDragOver = false;
+    if (event.dataTransfer && event.dataTransfer.files.length > 0) {
+      for (let i = 0; i < event.dataTransfer.files.length; i++) {
+        this.files.push(event.dataTransfer.files[i]);
+      }
+    }
+  }
+
+  onDragOver(event: DragEvent) {
+    event.preventDefault();
+    this.isDragOver = true;
+  }
+
+  onDragLeave(event: DragEvent) {
+    event.preventDefault();
+    this.isDragOver = false;
+  }
+}
