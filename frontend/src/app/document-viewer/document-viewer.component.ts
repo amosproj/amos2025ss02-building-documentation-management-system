a787c4c5fc41a584dc4c8ab6c9a2c4bb5fbb4380
vi55zebe <arpita.halder@fau.de>
2025-04-28 16:54:03 +0200

    Add initial Angular components for document viewer, list, metadata panel and chatbot

---
+++ frontend/src/app/document-viewer/document-viewer.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-document-viewer',
+  imports: [],
+  templateUrl: './document-viewer.component.html',
+  styleUrl: './document-viewer.component.scss'
+})
+export class DocumentViewerComponent {
+
+}
