85426ed6cc053ef1e11f5248c27b9b475f412612
vi55zebe <arpita.halder@fau.de>
2025-04-28 16:54:03 +0200

    Add initial Angular components for document viewer, list, metadata panel and chatbot

---
+++ frontend/src/app/metadata-panel/metadata-panel.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-metadata-panel',
+  imports: [],
+  templateUrl: './metadata-panel.component.html',
+  styleUrl: './metadata-panel.component.scss'
+})
+export class MetadataPanelComponent {
+
+}
