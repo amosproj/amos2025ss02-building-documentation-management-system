0fa23a34fcad215c6323086ca42bee68bf27bc3b
Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>
2025-04-28 19:39:27 +0200

    Solved conflicts with dev_bastiane
    Signed-off-by: Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>

--- BitandBeam/frontend/src/app/app.component.spec.ts
+++ BitandBeam/frontend/src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'building-ui' title`, () => {
+  it(`should have the 'BUILD.ING' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('building-ui');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, building-ui');
+    expect(app.title).toEqual('BUILD.ING');
   });
 });
