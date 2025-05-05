147ee06bc565efa7479f9ba92e94a0998e157901
eugenie Bastiane Djamen <bastiane@MacBook-Pro-von-eugenie.local>
2025-05-05 09:40:03 +0200

    removed unecesaary changes made
    Signed-off-by: eugenie Bastiane Djamen <bastiane@MacBook-Pro-von-eugenie.local>

--- BitAndBeam/frontend/src/app/app.routes.ts
+++ BitAndBeam/frontend/src/app/app.routes.ts
@@ -1,15 +1,6 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { UploadFileComponent } from './upload-file/upload-file.component';
+import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'upload', component: UploadFileComponent },
-];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
-})
-export class AppRoutingModule {}
+]
