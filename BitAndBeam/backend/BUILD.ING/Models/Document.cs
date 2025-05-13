0b6b94c222d08b1662b66a30e46bd0feb0e80146
Masum <abidhasan20@gmail.com>
2025-05-13 21:28:44 +0200

    uploadby and filename column added

--- BitAndBeam/backend/BUILD.ING/Models/Document.cs
+++ BitAndBeam/backend/BUILD.ING/Models/Document.cs
@@ -4,8 +4,12 @@ public class Document
 {
     public int Id { get; set; }
     public string Title { get; set; } = string.Empty;
-    public string FileName { get; set; } = string.Empty;
     public string FilePath { get; set; } = string.Empty;
     public string GroupId { get; set; } = string.Empty;
     public DateTime UploadedAt { get; set; } = DateTime.UtcNow;
+    public string UploadedBy { get; set; } = string.Empty;
+
+    public string? FileName { get; set; }
+
+
 }
