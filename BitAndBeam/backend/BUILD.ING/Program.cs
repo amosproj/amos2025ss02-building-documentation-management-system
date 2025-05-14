3428430528d79ebe22330355f1a17d9171edd816
Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>
2025-05-14 08:37:02 +0200

    testing backend linting compliance check without pre-formatting and reduced strictness
    Signed-off-by: Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>

--- BitAndBeam/backend/BUILD.ING/Program.cs
+++ BitAndBeam/backend/BUILD.ING/Program.cs
@@ -58,7 +58,9 @@ app.Run();
 record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
 
 
-{public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+
+{
+    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
 }
 
 
