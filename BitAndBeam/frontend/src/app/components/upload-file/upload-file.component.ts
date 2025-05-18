d3ac1df25d42be1004e8c3d618eef3bc1e91479d
Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>
2025-05-18 22:20:47 +0200

    Revert "Merging  Merge and integrate Document and Building REST API features from Dev (#83) by masum and samuel"
    This reverts commit 43134de6bdf331fcc60a4662af5ac5605677c308, reversing
    changes made to 1f7ee1cc666f8d2f57bdcc8225f1ec0353bf32b3.
    
    Signed-off-by: Masudur Rahaman Kazi kamsur.rahaman@gmail.com

--- BitAndBeam/frontend/src/app/components/upload-file/upload-file.component.ts
+++ BitAndBeam/frontend/src/app/components/upload-file/upload-file.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ConfigService } from '../../config.service';
+
 import { AuthService } from '../../services/auth.service';
 import { RouterModule } from '@angular/router';
 
