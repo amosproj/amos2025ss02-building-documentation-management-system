4abf0ab966139f04e6cce8427e9e6f585cf4fc52
Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>
2025-05-21 10:42:01 +0200

    added Authorization to backend builder for production
    Signed-off-by: Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>

--- BitAndBeam/backend/BUILD.ING/Program.cs
+++ BitAndBeam/backend/BUILD.ING/Program.cs
@@ -13,6 +13,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddAuthorization();
+
 
 // Add health check service
 builder.Services.AddHealthChecks();
