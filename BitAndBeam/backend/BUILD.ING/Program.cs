6a113005cb845628b25b9dbb0a8aae40c04ee987
Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>
2025-05-21 10:18:06 +0200

    added MapControllers to backend for production
    Signed-off-by: Masudur Rahaman Kazi <kamsur.rahaman@gmail.com>

--- BitAndBeam/backend/BUILD.ING/Program.cs
+++ BitAndBeam/backend/BUILD.ING/Program.cs
@@ -29,6 +29,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.MapControllers();
+
 var summaries = new[]
 {
     "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
